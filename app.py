811d0f2733dfa82a409492afde237a1adae9e562
Adarsh Dubey <dubeyadarshmain@gmail.com>
2022-12-08 23:15:45 +0530

    Added all the necessary classes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 value_dictionary = {
     'Two': 2,
     'Three': 3,
@@ -8,9 +10,10 @@ value_dictionary = {
     'Eight': 8,
     'Nine': 9,
     'Ten': 10,
-    'Jack': 11,
-    'Queen': 12,
-    'King': 13
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10,
+    'Ace': 11
 }
 
 suits = ['Club', 'Jack', 'Spade', 'Hearts']
@@ -27,3 +30,68 @@ class Card:
 
     def __str__(self):
         return f'{self.rank} of {self.suit}'
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = ''
+
+        for card in self.cards:
+            deck_comp += "\n" + card.__str__()
+        return f"The deck has: {deck_comp}"
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        singe_card = self.cards.pop()
+        return singe_card
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += value_dictionary[card.rank]
+
+        if card.rank == 'Ace':
+            self.aces += 1
+    
+    def adjust_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -=1
+
+class Chip:
+    def __init__(self, total = 100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+def take_bet(chips):
+    while True:
+
+        try:
+            chips.bet = int(input("How many chips would you like to bet? "))
+        except:
+            print("Sorry, please provide a valid integer!")
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, you do not have enough chips! You have {} chips left!".format(chips.total))
+            else: 
+                break
